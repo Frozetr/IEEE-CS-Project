3074d826ad784104fa5bc16e8e83a1df3e26f47d
lahit1 <naifcanbasci@gmail.com>
2026-03-31 16:54:12 +0300

    Update structure of IEntity and it's sub-classes.

--- core/src/main/java/ieee/cs/isik/platformergaeme/game/Entity.java
+++ core/src/main/java/ieee/cs/isik/platformergaeme/game/Entity.java
@@ -1,6 +1,7 @@
 package ieee.cs.isik.platformergaeme.game;
 
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.utils.Disposable;
 import org.jetbrains.annotations.NotNull;
 
 
@@ -10,15 +11,15 @@ import org.jetbrains.annotations.NotNull;
  *
  * @see IEntity
  */
-public abstract class Entity extends IEntity {
+public abstract class Entity extends IEntity implements Disposable {
 
     /// Every entity should have a body so they can interact with each other physically
     @NotNull
     private final Body body;
 
-    public Entity(final int id, final float health, final float maxHealth, @NotNull final Body body) {
+    public Entity(final int id, final int type, final String name, final float health, final float maxHealth, @NotNull final Body body) {
         // Initialize the super class IEntity
-        super(id, health, maxHealth);
+        super(id, type, name, health, maxHealth);
 
         this.body = body;
     }
--- shared/src/main/java/ieee/cs/isik/platformergaeme/game/IEntity.java
+++ shared/src/main/java/ieee/cs/isik/platformergaeme/game/IEntity.java
@@ -9,14 +9,22 @@ public abstract class IEntity {
     /// It's a unique integer, Neither of Entities can share same id.
     public final int id;
 
+    /// Represents type of the entity
+    public final int type;
+
+    /// Represents name of the entity
+    public String name;
+
     /// Represents health of entity. When reaches 0, entity dies.
     public float health;
 
     /// Represents maxHealth of entity. If it's 0, that means entity is immortal.
     public float maxHealth;
 
-    public IEntity(final int id, final float health, final float maxHealth) {
+    public IEntity(final int id, final int type, final String name, final float health, final float maxHealth) {
         this.id = id;
+        this.type = type;
+        this.name = name;
         this.health = health;
         this.maxHealth = maxHealth;
     }
