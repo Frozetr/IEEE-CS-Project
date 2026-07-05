f2c42cec18eb48f863a7641da6abd5c943a7c519
lahit1 <naifcanbasci@gmail.com>
2026-07-05 20:18:14 +0300

    refactor: Decouple Entity from IEntity
    This commit removes the inheritance of the `Entity` class from `IEntity`.
    
    This change decouples the client-side `Entity` from the shared `IEntity`, providing a clearer separation of concerns and allowing for more flexible and independent development of each class.

--- core/src/main/java/ieee/cs/isik/platformergaeme/game/entity/Entity.java
+++ core/src/main/java/ieee/cs/isik/platformergaeme/game/entity/Entity.java
@@ -2,17 +2,28 @@ package ieee.cs.isik.platformergaeme.game.entity;
 
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.utils.Disposable;
-import ieee.cs.isik.platformergaeme.game.IEntity;
 import ieee.cs.isik.platformergaeme.game.materials.Material;import org.jetbrains.annotations.NotNull;
 
 
 /**
  * This class represents any dynamic object in the game that going to be or has been registered to a server.
- * This class is inherited from {@link IEntity} and has extra data that required by client side.
- *
- * @see IEntity
  */
-public abstract class Entity extends IEntity implements Disposable {
+public abstract class Entity implements Disposable {
+
+    /// It's a unique integer, Neither of Entities can share same id.
+    public final int id;
+
+    /// Represents type of the entity
+    public final int type;
+
+    /// Represents name of the entity
+    public String name;
+
+    /// Represents health of entity. When reaches 0, entity dies.
+    public float health;
+
+    /// Represents maxHealth of entity. If it's 0, that means entity is immortal.
+    public float maxHealth;
 
     /// Every entity should have a body so they can interact with each other physically
     @NotNull
@@ -25,8 +36,11 @@ public abstract class Entity extends IEntity implements Disposable {
 
     public Entity(final int id, final int type, final String name, final float health, final float maxHealth, @NotNull final Body body, @NotNull Material material) {
         // Initialize the super class IEntity
-        super(id, type, name, health, maxHealth);
-
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.health = health;
+        this.maxHealth = maxHealth;
         this.body = body;
         this.material = material;
     }
